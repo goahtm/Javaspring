3ed808522e2a181cf5a5e1c9a9708de6b8f0ef64
fjru0505 <fjru0505@gmail.com>
2023-10-10 17:35:25 +0900

    This is Commit Message!

---
+++ src/Package01/Testgithub02.java
@@ -0,0 +1,7 @@
+package Package01;
+
+public class Testgithub02 {
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
---
+++ src/Package01/TestjavaGit.java
@@ -0,0 +1,8 @@
+package Package01;
+
+public class TestjavaGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+	}
+}
