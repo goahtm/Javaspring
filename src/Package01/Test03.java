ee38dcfeb3cd38bc8cee8a131d5de437832cfafb
fjru0505 <fjru0505@gmail.com>
2023-10-11 09:33:19 +0900

    commit

--- src/Package01/Test03.java
+++ src/Package01/Test03.java
@@ -2,6 +2,6 @@ package Package01;
 
 public class Test03 {
 	public static void main(String[] args) {
-		System.out.println("This is Github Commit Test");
+		System.out.println("This is Github Commit");
 	}
 }
